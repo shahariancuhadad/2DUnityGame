cb6eab53c5a84b21c8c34ff67e75a9c53ebb4f42
shahariancuhadad <70977065+shahariancuhadad@users.noreply.github.com>
2022-03-18 12:29:29 +0200

    assetplacement
    placing the assets such as character, goals, ball and barriers

---
+++ Game/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    private float moveSpeed;
+    private float jumpForce;
+    private bool isJumping;
+    private float moveHorizontal;
+    private float moveVertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+
+        moveSpeed = 3f;
+        jumpForce = 2f;
+        isJumping = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        //move character
+        if (moveHorizontal > 0.1f || moveHorizontal < -0.1f)
+        {
+            rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
+        }
+
+        if (moveVertical > 0.1f)
+        {
+            rb2D.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
